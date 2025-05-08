0b7859ebc1e8527406f642860c1255ae004db453
kozistr <kozistr@gmail.com>
2025-05-09 08:18:44 +0900

    update: search

--- sqb/search.py
+++ sqb/search.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
-import clip
-import cv2
+import numpy as np
+import open_clip
 import torch
 from PIL import Image
 from torch.nn.functional import normalize
@@ -9,34 +9,39 @@ from torch.nn.functional import normalize
 from sqb.tools import extract_character, remove_empty_area
 
 DEVICE: str = 'cuda' if torch.cuda.is_available() else 'cpu'
-BACKBONE: str = 'ViT-L/14'
-MODEL, PREPROCESS = clip.load(BACKBONE, device='cuda' if torch.cuda.is_available() else 'cpu')
+RECIPE: tuple[str, str] = ('ViT-L-14-quickgelu', 'dfn2b')
+
+MODEL, _, PREPROCESS = open_clip.create_model_and_transforms(RECIPE[0], pretrained=RECIPE[1], device=DEVICE)
+MODEL.eval()
 
 
-def load_model(backbone: str = 'ViT-L/14'):
-    return clip.load(backbone, device=DEVICE)
+def load_model(recipe: tuple[str, str] = ('ViT-L-14-quickgelu', 'dfn2b')):
+    model, _, preprocess = open_clip.create_model_and_transforms(recipe[0], pretrained=recipe[1], device=DEVICE)
+    model.eval()
+
+    return model, preprocess
 
 
 @torch.inference_mode()
-def get_image_embedding(image_path: Path | str) -> torch.Tensor:
-    image = PREPROCESS(Image.open(image_path)).unsqueeze(0).to(DEVICE)
+def get_image_embedding(image: torch.Tesnor) -> torch.Tensor:
+    image = PREPROCESS(image).unsqueeze(0).to(DEVICE)
     return normalize(MODEL.encode_image(image), p=2.0)
 
 
-def get_query_embedding(query_image_path: Path | str) -> torch.Tensor:
-    query_image = remove_empty_area(extract_character(query_image_path))
-    cv2.imwrite('query.png', query_image)
-
-    return get_image_embedding('query.png').reshape(1, -1)
+def get_query_embedding(query_image_path: Path | str) -> tuple[torch.Tensor, np.ndarray]:
+    query_image = extract_character(query_image_path)
+    query_image = remove_empty_area(query_image)
+    query_image = Image.fromarray(query_image[..., ::-1])
+    return get_image_embedding(query_image).reshape(1, -1), query_image
 
 
 def get_document_embeddings(document_path: Path | str) -> torch.Tensor:
     document_images: list[Path] = sorted(Path(document_path).glob('*/*.png'))
-    return torch.cat([get_image_embedding(path) for path in document_images], dim=0)
+    return torch.cat([get_image_embedding(Image.open(path)) for path in document_images], dim=0)
 
 
 def get_similar_images(query_image_path: Path | str, documents_embeddings: torch.Tensor, top_k: int = 5) -> list[int]:
-    query_embedding = get_query_embedding(query_image_path)
+    query_embedding, _ = get_query_embedding(query_image_path)
 
     similarity = (100.0 * query_embedding @ documents_embeddings.T).softmax(dim=-1, dtype=torch.float32)
 
